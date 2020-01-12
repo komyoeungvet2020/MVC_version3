17e90804ca626c6c1844db2ace065fed9ba5bf1a
Komyoeung vet <khomyoeung.vet711@gmail.com>
2020-01-12 15:07:56 +0700

    version3

---
+++ Views/students/edit.php
@@ -0,0 +1,69 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+        <div class="card">
+            <div class="card-header bg-primary text-center text-white">
+                <h2>Student Information Form</h2>
+            </div>
+            <?php
+            if(isset($data['student_data'])){
+                foreach($data['student_data'] as $rows){
+            ?>
+            <form action="index.php?action=edit_data&id=<?php echo $rows['id']; ?>" method="post">
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="fname">First Name:</label>
+                        <input type="text" name="fname" id="fname" class="form-control" value="<?php echo $rows['firstname'];?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="lname">Last Name:</label>
+                        <input type="text" name="lname" id="lname" class="form-control" value="<?php echo $rows['lastname'];?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="sex">Gender:</label><br>
+                        <input type="radio" <?php if($rows['sex']=="Male"){?> checked ="checked" <?php } ?> name="sex" value="Male">Male <br>
+                        <input type="radio" <?php if($rows['sex']=="Female"){?> checked ="checked" <?php } ?> name="sex" value="Female">Female
+                    </div>
+
+                    <div class="form-group">
+                        <label for="class">Class:</label>
+                        <select name="class" id="class" class="form-control">
+                            <?php
+                            foreach ($data['class_data'] as $row) { ?>
+                                <option <?php if($rows['title']== $row['title']){?> selected="selected"<?php }?> value="<?php echo $row['id']; ?>"><?php echo $row['title']; ?></option>
+                            <?php
+                            }
+                            ?>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="subject">Subjects:</label>
+                        <select name="subjects[]"  class="form-control" multiple>
+                            <?php
+                            
+                            foreach ($data['subject_data'] as $row) {
+                                foreach ($rows['sub_title'] as $title) {
+                                    ?>
+                                    <option <?php if($title == $row['sub_title']){?> selected="selected"<?php }?> value="<?php echo $row['id']; ?>"><?php echo $row['sub_title']; ?></option>
+                                <?php
+                                    }
+                                }
+                                ?>
+                                
+                        </select>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <a href="index.php?action=view" class="btn btn-success">Go Back</a>
+                    <input type="submit" name="create" value="Submit" class="btn btn-primary float-right">
+                </div>
+            </form>
+            <?php
+                }
+            }
+            ?>
+        </div>
+        </div>
+    </div>
+</div>
